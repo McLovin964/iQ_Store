af99d338956ee0fcfa10afd1371af0449983f76e
McLovin964 <DeathGrin_964@live.com>
2025-09-28 00:38:08 +0300

    Ready for Render deployment

--- server.js
+++ server.js
@@ -429,7 +429,7 @@ app.get(/^\/admin\/.*$/, (req, res) => {
 
 // Serve main SPA
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
+app.get(/^\/(?!api\/).*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
